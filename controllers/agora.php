23246553eb1888bd51604592d37d768d72134353
rended <renato.dedonato@gmail.com>
2017-02-01 13:26:41 +0100

    cos cos

--- controllers/agora.php
+++ controllers/agora.php
@@ -5,5 +5,8 @@ class SPODAGORA_CTRL_Agora extends OW_ActionController
     public function index()
     {
         OW::getDocument()->getMasterPage()->setTemplate(OW::getPluginManager()->getPlugin('spodagora')->getRootDir() . 'master_pages/empty.html');
+
+        OW::getDocument()->addScript(OW::getPluginManager()->getPlugin('spodagora')->getStaticJsUrl() . 'perfect-scrollbar.jquery.js');
+        OW::getDocument()->addStyleSheet(OW::getPluginManager()->getPlugin('spodagora')->getStaticCssUrl() . 'perfect-scrollbar.min.css');
     }
 }
