9e9dffa267153b5d8ae879e57f2280ddfe2556e8
andrpet <andrpet@gmail.com>
2017-04-07 15:58:59 +0200

    agora enhancement

--- components/nested_comment.php
+++ components/nested_comment.php
@@ -19,7 +19,7 @@ class SPODAGORA_CMP_NestedComment extends OW_Component
     {
         $users_ids      = array_map(function($comments) { return $comments->ownerId;}, $comments);
         $user_id        = $this->userId;
-        $this->avatars  = BOL_AvatarService::getInstance()->getDataForUserAvatars($users_ids);
+        $this->avatars  = SPODAGORA_CLASS_Tools::getInstance()->process_avatar(BOL_AvatarService::getInstance()->getDataForUserAvatars($users_ids));
         $this->assign('avatars', $this->avatars);
 
         $today = date('Ymd');
@@ -27,11 +27,13 @@ class SPODAGORA_CMP_NestedComment extends OW_Component
 
         foreach ($comments as &$comment)
         {
-            $comment->username      = $this->avatars[$comment->ownerId]["title"];
-            $comment->owner_url     = $this->avatars[$comment->ownerId]["url"];
-            $comment->avatar_url    = $this->avatars[$comment->ownerId]["src"];
-            $comment->total_comment = isset($comment->total_comment) ? $comment->total_comment : 0;
-            $comment->timestamp     = $this->process_timestamp($comment->timestamp, $today, $yesterday);
+            $comment->username       = $this->avatars[$comment->ownerId]["title"];
+            $comment->owner_url      = $this->avatars[$comment->ownerId]["url"];
+            $comment->avatar_url     = $this->avatars[$comment->ownerId]["src"];
+            $comment->avatar_css     = $this->avatars[$comment->ownerId]["css"];
+            $comment->avatar_initial = $this->avatars[$comment->ownerId]["initial"];
+            $comment->total_comment  = isset($comment->total_comment) ? $comment->total_comment : 0;
+            $comment->timestamp      = SPODAGORA_CLASS_Tools::getInstance()->process_timestamp($comment->timestamp, $today, $yesterday);
 
             $comment->css_class       = $user_id == $comment->ownerId ? 'agora_right_comment' : 'agora_left_comment';
             $comment->sentiment_class = $comment->sentiment == 0 ? 'neutral' : ($comment->sentiment == 1 ? 'satisfied' : 'dissatisfied');
@@ -60,23 +62,9 @@ class SPODAGORA_CMP_NestedComment extends OW_Component
             $html .= $key."='".$value."' ";
         }
 
-        //$html .= " fields='[".$comment->fields."]' ";
         $html .= " ></{$comment->component}>";
 
         return $html;
-    }
-
-    private function process_timestamp($timestamp, $today, $yesterday)
-    {
-        $date = date('Ymd', strtotime($timestamp));
-
-        if($date == $today)
-            return date('H:i', strtotime($timestamp));
-
-        if($date == $yesterday)
-            return OW::getLanguage()->text('spodagora', 'yesterday'). " " . date('H:i', strtotime($timestamp));
-
-        return date('H:i m/d', strtotime($timestamp));
     }
 
 }
